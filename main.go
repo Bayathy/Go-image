1981574664a9472adc8ff7e3f6a86c5ed07b9e91
Bayathy <misosoup0321@icloud.com>
2021-12-21 21:01:54 +0900

    add if condition

--- main.go
+++ main.go
@@ -15,40 +15,31 @@ func main() {
 
 	wtclist := []color.RGBA{}
 
-	var i uint8
+	var (
+		i      uint8
+		j      uint8
+		n      uint8
+		crange uint8
+		srange uint8
+	)
 
-	for num := 0; num < 3; num++ {
-		for i = 0; i < 120; i++ {
-			switch num {
-			case 0:
-				wtcc := color.RGBA{60 + i, 60, 60, 255}
-				wtclist = append(wtclist, wtcc)
-			case 1:
-				wtcc := color.RGBA{60, 60 + i, 60, 255}
-				wtclist = append(wtclist, wtcc)
-			case 2:
-				wtcc := color.RGBA{60, 60, 60 + i, 255}
+	crange = 20
+	srange = 70
+
+	for i = 0; i < crange; i++ {
+		for j = 0; j < crange; j++ {
+			for n = 0; n < crange; n++ {
+				wtcc := color.RGBA{srange + i, srange + j, srange + n, 255}
+
 				wtclist = append(wtclist, wtcc)
 			}
 		}
 	}
 
 	aclist := [...]color.RGBA{
-		{226, 71, 29, 255},
-		{230, 20, 14, 255},
-		{230, 99, 82, 255},
-		{255, 255, 255, 255},
-		{246, 158, 44, 255},
-		{222, 196, 67, 255},
-		{212, 170, 109, 255},
-		{255, 255, 255, 255},
-		{154, 123, 85, 255},
-		{218, 139, 64, 255},
-		{208, 132, 104, 255},
-		{255, 255, 255, 255},
+		//{170,94,102,255},
+		{170, 44, 48, 255},
 	}
-
-	_ = aclist
 
 	img, _ := png.Decode(in)
 	bounds := img.Bounds()
@@ -62,7 +53,7 @@ func main() {
 		for x := bounds.Min.X; x < bounds.Max.X; x++ {
 			color := img.At(x, y)
 			for i := 0; i < len(wtclist); i++ {
-				if color == wtclist[i] {
+				if color == wtclist[i] && y < 400 {
 					//fmt.Println("success")
 					color = aclist[rand.Intn(len(aclist))]
 				}
