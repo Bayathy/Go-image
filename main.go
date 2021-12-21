eb902adbe2c9a83500250d327d7de4a897d3189a
Bayathy <misosoup0321@icloud.com>
2021-12-21 16:48:22 +0900

    test color model

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
 
 import (
-	"image"
-	"image/color"
+	"fmt"
+	_ "image"
+	_ "image/color"
 	"image/png"
 	"os"
 )
 
 func main() {
 	img, _ := png.Decode(os.Stdin)
-	bounds := img.Bounds()
-	dest := image.NewGray16(bounds)
-	for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
-		for x := bounds.Min.X; x < bounds.Max.X; x++ {
-			c := color.Gray16Model.Convert(img.At(x, y))
-			gray, _ := c.(color.Gray16)
-			dest.Set(x, y, gray)
-		}
-	}
-	png.Encode(os.Stdout, dest)
+	//bounds := img.Bounds()
+	fmt.Println(img.ColorModel()) 
+
+
+
+	//dest := image.NewGray16(bounds)
+	//for y := bounds.Min.Y; y < bounds.Max.Y; y++ {
+	//	for x := bounds.Min.X; x < bounds.Max.X; x++ {
+	//		c := color.Gray16Model.Convert(img.At(x, y))
+	//		gray, _ := c.(color.Gray16)
+	//		dest.Set(x, y, gray)
+	//	}
+	//}
+	//png.Encode(os.Stdout, dest)
 }
